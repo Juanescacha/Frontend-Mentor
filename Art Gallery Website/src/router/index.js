bbb8685b2711ab26431a3105b98a5ca830be24b7
Juan Esteban Camargo <juanescacha@hotmail.com>
2023-09-12 15:05:57 -0500

    router init

---
+++ Art Gallery Website/src/router/index.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from "vue-router"
+
+import MainView from "../views/MainPage.vue"
+import LocationView from "../views/LocationView.vue"
+
+const routes = [
+	{
+		path: "/",
+		component: MainView,
+		name: "Main",
+	},
+	{
+		path: "/location",
+		component: LocationView,
+		name: "Location",
+	},
+]
+
+const router = createRouter({
+	history: createWebHistory(),
+	routes,
+})
+
+export default router
