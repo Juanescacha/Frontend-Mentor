0963717fcbeaf401bf746583ba4c6dce1778e355
Juan Esteban Camargo <juanescacha@hotmail.com>
2023-09-13 09:21:28 -0500

    styles definition

--- Art Gallery Website/src/views/MainView.vue
+++ Art Gallery Website/src/views/MainView.vue
@@ -1,5 +1,18 @@
 <script setup></script>
 
 <template>
-	<div class="h-screen w-full bg-fem-gold text-9xl">Text test</div>
+	<div class="w-full flex flex-col items-start p-60">
+		<div class="headingXL">Heading XL</div>
+		<a
+			class="mt-20 flex"
+			href="#">
+			<div class="buttom">Our location</div>
+			<div
+				class="w-14 h-[72px] bg-fem-gold flex items-center justify-center">
+				<img
+					src="../assets/icon-arrow-right.svg"
+					alt="arrow-right" />
+			</div>
+		</a>
+	</div>
 </template>
