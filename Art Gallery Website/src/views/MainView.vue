ac4ac8073c505a6f635520de8d59244566e678bc
Juan Esteban Camargo <juanescacha@hotmail.com>
2023-09-14 09:44:30 -0500

    advances

--- Art Gallery Website/src/views/MainView.vue
+++ Art Gallery Website/src/views/MainView.vue
@@ -1,10 +1,35 @@
 <script setup></script>
 
 <template>
-	<div class="w-full flex flex-col items-start p-60">
-		<div class="headingXL">Heading XL</div>
-		<a
-			class="mt-20 flex"
+	<div class="flex flex-col items-center justify-center w-full">
+		<div class="bg-red-500 w-full h-[630px] flex flex-col items-center">
+			<img
+				src="../assets/mobile/image-hero.jpg"
+				alt="HeroImage" />
+			<div class="flex flex-col w-full gap-8 px-4 mt-8 bg-green-500">
+				<h1 class="headingM leading-[55px] self-start">
+					MODERN<br />ART GALLERY
+				</h1>
+				<p class="bodyS text-fem-darkGrey">
+					The arts in the collection of the Modern Art Gallery all
+					started from a spark of inspiration. Will these pieces
+					inspire you? Visit us and find out.
+				</p>
+				<a
+					class="flex"
+					href="#">
+					<div class="buttom">Our location</div>
+					<div
+						class="w-14 h-[72px] bg-fem-gold flex items-center justify-center">
+						<img
+							src="../assets/icon-arrow-right.svg"
+							alt="arrow-right" />
+					</div>
+				</a>
+			</div>
+		</div>
+		<!-- <a
+			class="flex mt-20"
 			href="#">
 			<div class="buttom">Our location</div>
 			<div
@@ -13,6 +38,6 @@
 					src="../assets/icon-arrow-right.svg"
 					alt="arrow-right" />
 			</div>
-		</a>
+		</a> -->
 	</div>
 </template>
