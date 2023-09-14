f75091c700f18ebae7b83b403e8385e794431d4d
Juan Esteban Camargo <juanescacha@hotmail.com>
2023-09-14 15:44:23 -0500

    Buttom component

---
+++ Art Gallery Website/src/components/Button.vue
@@ -0,0 +1,23 @@
+<script setup>
+	const props = defineProps(["text", "direction"])
+</script>
+
+<template>
+	<a
+		class="flex h-[72px] group"
+		href="#">
+		<div class="buttonBase">{{ text }}</div>
+		<div
+			class="buttonRibbon"
+			:class="{ 'order-first': direction == 'left' }">
+			<img
+				src="../assets/icon-arrow-right.svg"
+				alt="icon-arrow-right"
+				v-if="direction === 'right'" />
+			<img
+				src="../assets/icon-arrow-left.svg"
+				alt="icon-arrow-left"
+				v-if="direction === 'left'" />
+		</div>
+	</a>
+</template>
