0fc8783bd68713b1a85546c25320df60f48c4437
Juan Esteban Camargo <juanescacha@hotmail.com>
2023-09-16 13:12:20 -0500

    mainview for tablet

--- Art Gallery Website/src/views/MainView.vue
+++ Art Gallery Website/src/views/MainView.vue
@@ -4,16 +4,24 @@
 
 <template>
 	<div class="flex flex-col items-center justify-center w-full gap-[120px]">
-		<div class="flex flex-col items-center">
+		<div class="flex flex-col items-center md:flex-row">
+			<!-- mobile hero image -->
 			<img
 				src="/src/assets/mobile/image-hero.jpg"
 				alt="HeroImage"
-				class="w-screen" />
-			<div class="flex flex-col w-full gap-8 px-4 mt-8">
-				<h1 class="headingM leading-[55px] self-start">
+				class="w-screen md:hidden" />
+			<!-- tablet hero image -->
+			<img
+				src="/src/assets/tablet/image-hero.jpg"
+				alt="HeroImage"
+				class="hidden w-full md:block lg:hidden" />
+			<div
+				class="flex flex-col w-full gap-8 md:gap-12 px-4 md:px-0 mt-8 md:mt-0 md:-ml-[47px] md:mr-10">
+				<h1
+					class="headingM leading-[55px] md:headingL self-start md:leading-[65px] md:w-full">
 					MODERN<br />ART GALLERY
 				</h1>
-				<p class="bodyS text-fem-darkGrey">
+				<p class="bodyS text-fem-darkGrey md:w-[339px]">
 					The arts in the collection of the Modern Art Gallery all
 					started from a spark of inspiration. Will these pieces
 					inspire you? Visit us and find out.
@@ -24,12 +32,21 @@
 					route="/location" />
 			</div>
 		</div>
-		<div class="flex flex-col w-full gap-8">
-			<div class="flex flex-col gap-[25px] px-4">
+		<div class="flex flex-col w-full gap-8 md:gap-[11px] md:px-10">
+			<div
+				class="flex flex-col gap-[25px] px-4 md:px-0 md:flex-row-reverse md:justify-between md:items-center">
+				<!-- mobile image 1 -->
 				<img
 					src="/src/assets/mobile/image-grid-1.jpg"
-					alt="image1" />
-				<div class="flex flex-col gap-[21px]">
+					alt="image1"
+					class="md:hidden" />
+				<!-- tablet image 1 -->
+				<img
+					src="/src/assets/tablet/image-grid-1.jpg"
+					alt="image1"
+					class="hidden md:block lg:hidden" />
+				<div
+					class="flex flex-col gap-[21px] md:w-[223px] md:gap-6 md:h-full">
 					<h2 class="headingS text-[50px] leading-[45px]">
 						Your Day <br />
 						at the Gallery
@@ -41,16 +58,31 @@
 					</p>
 				</div>
 			</div>
-			<div class="flex flex-col gap-4 px-4">
+			<div
+				class="flex flex-col gap-4 px-4 md:flex-row md:px-0 md:gap-[11px]">
+				<!-- mobile image 2 -->
 				<img
 					src="/src/assets/mobile/image-grid-2.jpg"
-					alt="image1" />
-				<div class="flex flex-col gap-4">
+					alt="image1"
+					class="md:hidden" />
+				<!-- tablet image 2 -->
+				<img
+					src="/src/assets/tablet/image-grid-2.jpg"
+					alt="image1"
+					class="hidden md:block lg:hidden" />
+				<div class="flex flex-col gap-4 md:gap-[11px]">
+					<!-- mobile image 3 -->
 					<img
 						src="/src/assets/mobile/image-grid-3.jpg"
-						alt="image3" />
+						alt="image3"
+						class="md:hidden" />
+					<!-- tablet image 3 -->
+					<img
+						src="/src/assets/tablet/image-grid-3.jpg"
+						alt="image3"
+						class="hidden md:block lg:hidden" />
 					<div
-						class="flex flex-col gap-6 px-6 py-12 bg-fem-almostBlack">
+						class="flex flex-col gap-6 px-6 py-12 bg-fem-almostBlack md:h-full md:py-[85px] md:pl-[29px] md:pr-7">
 						<h3
 							class="headingS text-[50px] leading-[45px] text-white">
 							COME & BE INSPIRED
@@ -64,17 +96,18 @@
 			</div>
 		</div>
 		<div
-			class="flex flex-col px-8 py-12 bg-fem-almostBlack gap-[38px] w-full">
+			class="flex flex-col px-8 py-12 bg-fem-almostBlack gap-[38px] w-full md:flex-row md:pl-[39px] md:pr-10 md:py-14 md:justify-between md:items-start md:gap-0">
 			<img
 				src="/src/assets/logo-light.svg"
 				alt="logo-light"
 				class="w-fit" />
-			<p class="bodyS text-base leading-[26px] text-white">
+			<p
+				class="bodyS text-base leading-[26px] text-white md:w-[281px] md:ml-[64.95px] md:mr-[133px]">
 				The Modern Art Gallery is free to all visitors and open seven
 				days a week from 8am to 9pm. Find us at 99 King Street, Newport,
 				USA.
 			</p>
-			<div class="flex w-[102px] justify-between">
+			<div class="flex w-[102px] md:w-[100px] justify-between">
 				<img
 					src="/src/assets/icon-facebook.svg"
 					alt="icon-facebook" />
