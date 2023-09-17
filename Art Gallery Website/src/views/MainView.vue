9047c3e9c93717232d2c23f771ec43de6e75c1ac
Juan Esteban Camargo <juanescacha@hotmail.com>
2023-09-17 12:34:47 -0500

    change from headwind to prettier-tailwindcss plugin to sort classes

--- Art Gallery Website/src/views/MainView.vue
+++ Art Gallery Website/src/views/MainView.vue
@@ -5,17 +5,17 @@
 
 <template>
 	<div
-		class="flex flex-col items-center justify-center w-full gap-[120px] lg:gap-[180px]">
+		class="flex w-full flex-col items-center justify-center gap-[120px] lg:gap-[180px]">
 		<div
-			class="relative flex flex-col items-center md:flex-row md:w-full lg:pr-[165px]">
+			class="relative flex flex-col items-center md:w-full md:flex-row lg:pr-[165px]">
 			<h1
-				class="z-20 absolute hidden text-white mix-blend-difference lg:block headingXL w-fit left-[165px] top-[189px]">
+				class="headingXL absolute left-[165px] top-[189px] z-20 hidden w-fit text-white mix-blend-difference lg:block">
 				MODERN<br />ART GALLERY
 			</h1>
 			<div
-				class="z-10 self-stretch hidden w-full bg-fem-almostBlack lg:block"></div>
+				class="z-10 hidden w-full self-stretch bg-fem-almostBlack lg:block"></div>
 			<h1
-				class="z-0 absolute hidden text-white lg:block headingXL w-fit left-[165px] top-[189px]">
+				class="headingXL absolute left-[165px] top-[189px] z-0 hidden w-fit text-white lg:block">
 				MODERN<br />ART GALLERY
 			</h1>
 			<!-- mobile hero image -->
@@ -35,13 +35,13 @@
 				class="hidden w-full lg:block" />
 
 			<div
-				class="flex flex-col w-full gap-8 md:gap-12 lg:gap-16 px-4 md:px-0 mt-8 md:mt-0 md:-ml-[47px] md:mr-10 lg:mr-0 lg:self-start lg:top-[190px] relative lg:w-fit lg:-ml-[65px]">
+				class="relative mt-8 flex w-full flex-col gap-8 px-4 md:-ml-[47px] md:mr-10 md:mt-0 md:gap-12 md:px-0 lg:top-[190px] lg:-ml-[65px] lg:mr-0 lg:w-fit lg:gap-16 lg:self-start">
 				<h1
-					class="headingM leading-[55px] md:headingL self-start md:leading-[65px] md:w-full lg:hidden">
+					class="headingM md:headingL self-start leading-[55px] md:w-full md:leading-[65px] lg:hidden">
 					MODERN<br />ART GALLERY
 				</h1>
 				<p
-					class="bodyS text-fem-darkGrey md:w-[339px] lg:w-[350px] lg:bodyM">
+					class="bodyS lg:bodyM text-fem-darkGrey md:w-[339px] lg:w-[350px]">
 					The arts in the collection of the Modern Art Gallery all
 					started from a spark of inspiration. Will these pieces
 					inspire you? Visit us and find out.
@@ -53,9 +53,9 @@
 			</div>
 		</div>
 		<div
-			class="flex flex-col w-full gap-8 md:gap-[11px] md:px-10 lg:px-[164px] lg:gap-[30px]">
+			class="flex w-full flex-col gap-8 md:gap-[11px] md:px-10 lg:gap-[30px] lg:px-[164px]">
 			<div
-				class="flex flex-col gap-[25px] px-4 md:px-0 md:flex-row-reverse md:justify-between md:items-center">
+				class="flex flex-col gap-[25px] px-4 md:flex-row-reverse md:items-center md:justify-between md:px-0">
 				<!-- mobile image 1 -->
 				<img
 					src="/src/assets/mobile/image-grid-1.jpg"
@@ -71,14 +71,15 @@
 					src="/src/assets/desktop/image-grid-1.jpg"
 					alt="image1"
 					class="hidden lg:block" />
+
 				<div
-					class="flex flex-col gap-[21px] md:w-[223px] lg:w-[350px] md:gap-6 md:h-full lg:gap-8">
+					class="flex flex-col gap-[21px] md:h-full md:w-[223px] md:gap-6 lg:w-[350px] lg:gap-8">
 					<h2
-						class="headingS text-[50px] leading-[45px] lg:headingM lg:leading-[60px]">
+						class="headingS lg:headingM text-[50px] leading-[45px] lg:leading-[60px]">
 						Your Day <br class="lg:hidden" />
 						at the Gallery
 					</h2>
-					<p class="bodyS text-fem-darkGrey lg:bodyM">
+					<p class="bodyS lg:bodyM text-fem-darkGrey">
 						Wander through our distinct collections and find new
 						insights about our artists. Dive into the details of
 						their creative process.
@@ -86,7 +87,7 @@
 				</div>
 			</div>
 			<div
-				class="flex flex-col gap-4 px-4 md:flex-row md:px-0 md:gap-[11px] md:justify-center md:w-full lg:gap-[30px]">
+				class="flex flex-col gap-4 px-4 md:w-full md:flex-row md:justify-center md:gap-[11px] md:px-0 lg:gap-[30px]">
 				<!-- mobile image 2 -->
 				<img
 					src="/src/assets/mobile/image-grid-2.jpg"
@@ -119,12 +120,12 @@
 						alt="image3"
 						class="hidden lg:block" />
 					<div
-						class="flex flex-col gap-6 px-6 py-12 bg-fem-almostBlack md:h-full md:py-[85px] md:pl-[29px] md:pr-7 lg:gap-8 lg:pl-12 lg:pr-[47px] lg:pt-[65px] lg:pb-16">
+						class="flex flex-col gap-6 bg-fem-almostBlack px-6 py-12 md:h-full md:py-[85px] md:pl-[29px] md:pr-7 lg:gap-8 lg:pb-16 lg:pl-12 lg:pr-[47px] lg:pt-[65px]">
 						<h3
-							class="headingS text-[50px] leading-[45px] text-white lg:headingM lg:leading-[60px] lg:text-white">
+							class="headingS lg:headingM text-[50px] leading-[45px] text-white lg:leading-[60px] lg:text-white">
 							COME & BE INSPIRED
 						</h3>
-						<p class="text-white bodyS lg:bodyM lg:text-white">
+						<p class="bodyS lg:bodyM text-white lg:text-white">
 							We’re excited to welcome you to our gallery and see
 							how our collections influence you.
 						</p>
