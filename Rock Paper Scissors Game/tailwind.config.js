4d1935d9155c1250874d9a79839c4ebfa6051633
Juan Esteban Camargo <juanescacha@hotmail.com>
2023-09-17 13:51:44 -0500

    Init and base configuration for new challenge "Rock Packer Scissors Lizard Spock Game"

---
+++ Rock Paper Scissors Game/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				fem: {
+					dark: "#3B4363",
+					score: "#2A46C0",
+					header: "#606E85",
+				},
+			},
+			fontFamily: {
+				sans: ["Barlow Semi Condensed", "sans-serif"],
+			},
+			fontSize: {
+				title: ["52px", "62px"],
+				body: ["18px", "28px"],
+			},
+			screens: {
+				sm: "375px",
+				lg: "1366px",
+			},
+		},
+	},
+	plugins: [],
+}
