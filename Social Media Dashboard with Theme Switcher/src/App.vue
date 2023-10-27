2bf54cc0ae007ae96e580874215820f929916384
Juan Esteban Camargo <juanescacha@hotmail.com>
2023-10-27 17:17:56 -0500

    refactor: semantic fix and added footer

--- Social Media Dashboard with Theme Switcher/src/App.vue
+++ Social Media Dashboard with Theme Switcher/src/App.vue
@@ -11,29 +11,60 @@
 	<HeaderBar
 		:title="title"
 		:totalFollowers="totalFollowers" />
-	<main class="main">
-		<section class="main__first-container">
+	<main class="app__main">
+		<section class="app__first-container">
 			<h2 class="sr-only">All Followers</h2>
 			<MainCard
 				v-for="card in followers"
 				:key="card.name"
 				:card="card" />
 		</section>
-		<section class="main__second-container">
-			<h2 class="main__subtitle">{{ title2 }}</h2>
+		<section class="app__second-container">
+			<h2 class="app__subtitle">{{ title2 }}</h2>
 			<SecondaryCard
 				v-for="card in todayOverview"
 				key="card.type"
 				:card="card" />
 		</section>
 	</main>
+	<footer class="app__attribution">
+		Challenge by
+		<a
+			href="https://www.frontendmentor.io?ref=challenge"
+			target="_blank"
+			>Frontend Mentor</a
+		>
+		Coded by
+		<a
+			href="https://github.com/Juanescacha"
+			target="_blank"
+			>Juan Camargo</a
+		>
+	</footer>
 </template>
 
 <style lang="scss">
 	@media (min-width: 0px) {
-		.main {
-			padding: 0 1.56rem 2.81rem 1.56rem;
-			margin-top: -2.75rem;
+		.app {
+			&__main {
+				padding: 0 1.56rem 2.81rem 1.56rem;
+				margin-top: -2.75rem;
+			}
+
+			&__attribution {
+				font-size: 1rem;
+				text-align: center;
+				color: var(--big-text);
+				margin-bottom: 3rem;
+
+				a {
+					color: var(--twitter);
+
+					&:hover {
+						text-decoration: underline;
+					}
+				}
+			}
 
 			&__subtitle {
 				color: var(--big-text);
@@ -58,11 +89,13 @@
 	}
 
 	@media (min-width: $small-breakpoint) {
-		.main {
-			padding-bottom: 4.69rem;
-			padding-left: 10.31rem;
-			padding-right: 10.31rem;
-			margin-top: -6.75rem;
+		.app {
+			&__main {
+				padding-bottom: 4.69rem;
+				padding-left: 10.31rem;
+				padding-right: 10.31rem;
+				margin-top: -6.75rem;
+			}
 
 			&__first-container {
 				flex-direction: row;
