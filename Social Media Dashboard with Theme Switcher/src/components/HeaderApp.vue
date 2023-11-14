75c054ae96a3d38db58371d32bf6ed0e0e8e59c0
Juan Esteban Camargo <juanescacha@hotmail.com>
2023-11-14 15:21:43 -0500

    feat: :wheelchair: enhance dark theme toggle to be more accesible and have better semantics

--- Social Media Dashboard with Theme Switcher/src/components/HeaderApp.vue
+++ Social Media Dashboard with Theme Switcher/src/components/HeaderApp.vue
@@ -7,11 +7,13 @@
 	})
 
 	const switchToggle = ref(true)
+	const darkMode = ref(true)
 
 	const handleToggle = () => {
 		switchToggle.value = !switchToggle.value
 		const body = document.querySelector("body")
 		body.classList.toggle("dark")
+		darkMode.value = !darkMode.value
 	}
 </script>
 
@@ -25,94 +27,111 @@
 				{{ totalFollowers.title + totalFollowers.number }}
 			</span>
 		</div>
-		<div
-			class="header__right"
-			id="header__label"
-			@click="handleToggle">
+		<div class="header__right">
 			<span class="header__label">Dark Mode</span>
-			<button
-				class="header__toggle"
-				type="button"
-				aria-labelledby="header__label"
-				role="switch"
-				:aria-checked="switchToggle">
-				<div
-					class="header__toggle-circle"
-					:class="{
-						'header__toggle-circle--active': switchToggle,
-					}"></div>
-			</button>
+			<label class="header__switch">
+				switch
+				<input
+					@change="handleToggle"
+					type="checkbox"
+					class="header__input"
+					:value="darkMode" />
+				<span class="header__slider"></span>
+			</label>
 		</div>
 	</header>
 </template>
 
 <style lang="scss" scoped>
-	@media (min-width: 0px) {
-		.header {
-			background-color: var(--top-bg);
-			padding: 2.25rem 1.56rem 5.25rem 1.56rem;
-			border-radius: 0rem 0rem 1.25rem 1.25rem;
+	.header {
+		background-color: var(--top-bg);
+		padding: 2.25rem 1.56rem 5.25rem 1.56rem;
+		border-radius: 0rem 0rem 1.25rem 1.25rem;
 
-			&__title {
+		&__title {
+			color: var(--big-text);
+			font-size: 1.5rem;
+			font-weight: 700;
+			text-transform: capitalize;
+			line-height: normal;
+		}
+		&__info {
+			font-weight: 700;
+			font-size: 0.875rem;
+			text-transform: capitalize;
+		}
+		&__label {
+			font-weight: 700;
+			font-size: 0.875rem;
+			text-transform: capitalize;
+			user-select: none;
+		}
+
+		&__switch {
+			position: relative;
+			display: inline-block;
+			font-size: 0.1px;
+			width: 3rem;
+			height: 1.5rem;
+		}
+
+		&__input {
+			opacity: 0;
+			width: 0;
+			height: 0;
+
+			&:focus + .header__slider {
+				outline: solid 3px var(--big-text);
+			}
+
+			&:checked + .header__slider:before {
+				transform: translateX(1.375rem);
+			}
+		}
+
+		&__slider {
+			position: absolute;
+			cursor: pointer;
+			top: 0;
+			left: 0;
+			right: 0;
+			bottom: 0;
+			background: var(--toggle);
+			transition: color 0.4s;
+			border-radius: 2.12rem;
+
+			&:before {
+				content: "";
+				position: absolute;
+				height: 1.125rem;
+				width: 1.125rem;
+				left: 0.25rem;
+				bottom: calc(50% - 1.125rem / 2);
+				background-color: var(--card);
+				border-radius: 50%;
+				transition: all 0.4s;
+			}
+		}
+
+		&__left {
+			display: flex;
+			flex-direction: column;
+			padding-bottom: 1.5rem;
+			gap: 0.25rem;
+			justify-content: center;
+			align-items: flex-start;
+			border-bottom: 0.0625rem solid var(--card-hover);
+		}
+		&__right {
+			display: flex;
+			flex-direction: row;
+			justify-content: space-between;
+			align-items: center;
+			margin-top: 1rem;
+
+			&:hover {
+				cursor: pointer;
 				color: var(--big-text);
-				font-size: 1.5rem;
-				font-weight: 700;
-				text-transform: capitalize;
-				line-height: normal;
-			}
-			&__info {
-				font-weight: 700;
-				font-size: 0.875rem;
-				text-transform: capitalize;
-			}
-			&__label {
-				font-weight: 700;
-				font-size: 0.875rem;
-				text-transform: capitalize;
-			}
-			&__toggle {
-				width: 3rem;
-				height: 1.5rem;
-				background: var(--toggle);
-				border-radius: 1.25rem;
-				position: relative;
-				&-circle {
-					background-color: var(--card);
-					position: absolute;
-					top: 50%;
-
-					transform: translate(1.688rem, -50%);
-					border-radius: 50%;
-					width: 1.125rem;
-					height: 1.125rem;
-					&--active {
-						transform: translate(0.188rem, -50%);
-					}
-				}
-				&:hover {
-					cursor: pointer;
-				}
-			}
-			&__left {
-				display: flex;
-				flex-direction: column;
-				padding-bottom: 1.5rem;
-				gap: 0.25rem;
-				justify-content: center;
-				align-items: flex-start;
-				border-bottom: 0.0625rem solid var(--card-hover);
-			}
-			&__right {
-				display: flex;
-				flex-direction: row;
-				justify-content: space-between;
-				align-items: center;
-				margin-top: 1rem;
-
-				&:hover {
-					cursor: pointer;
-					color: var(--big-text);
-				}
 			}
 		}
 	}
