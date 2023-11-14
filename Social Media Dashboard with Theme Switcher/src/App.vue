0b158a77d62696c34bd546bce99159d9b45ad0c3
Juan Esteban Camargo <juanescacha@hotmail.com>
2023-11-13 20:04:08 -0500

    refactor: :recycle: refactor and update MainApp component

--- Social Media Dashboard with Theme Switcher/src/App.vue
+++ Social Media Dashboard with Theme Switcher/src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
 	// components
-	import MainCard from "./components/MainCard.vue"
-	import SecondaryCard from "./components/SecondaryCard.vue"
+	import MainApp from "./components/MainApp.vue"
 	import HeaderApp from "./components/HeaderApp.vue"
 	import FooterApp from "./components/FooterApp.vue"
 
@@ -15,80 +14,11 @@
 	<HeaderApp
 		:title="title"
 		:totalFollowers="totalFollowers" />
-	<main class="app__main">
-		<section class="app__first-container">
-			<h2 class="sr-only">All Followers</h2>
-			<MainCard
-				v-for="card in followers"
-				:key="card.name"
-				:card="card" />
-		</section>
-		<section class="app__second-container">
-			<h2 class="app__subtitle">{{ title2 }}</h2>
-			<SecondaryCard
-				v-for="card in todayOverview"
-				key="card.type"
-				:card="card" />
-		</section>
-	</main>
+	<MainApp
+		:followers="followers"
+		:title="title2"
+		:todayOverview="todayOverview" />
 	<FooterApp />
 </template>
 
-<style lang="scss" scoped>
-	@media (min-width: 0px) {
-		.app {
-			&__main {
-				padding: 0 1.56rem 2.81rem 1.56rem;
-				margin-top: -2.75rem;
-			}
-
-			&__subtitle {
-				color: var(--big-text);
-				font-size: 1.5rem;
-				font-weight: 700;
-				text-transform: capitalize;
-				margin-top: 2.88rem;
-				margin-bottom: calc(1.69rem - 1rem);
-			}
-
-			&__first-container {
-				display: flex;
-				flex-direction: column;
-				gap: 1.5rem;
-			}
-			&__second-container {
-				display: flex;
-				flex-direction: column;
-				gap: 1rem;
-			}
-		}
-	}
-
-	@media (min-width: $small-breakpoint) {
-		.app {
-			&__main {
-				padding-bottom: 4.69rem;
-				padding-left: 10.31rem;
-				padding-right: 10.31rem;
-				margin-top: -6.75rem;
-			}
-
-			&__first-container {
-				flex-direction: row;
-				flex-wrap: wrap;
-				gap: 1.88rem;
-			}
-			&__second-container {
-				display: grid;
-				grid-template-columns: repeat(4, minmax(0, 1fr));
-				gap: 0;
-				row-gap: 1.5rem;
-				column-gap: 1.88rem;
-			}
-			&__subtitle {
-				grid-column: 1 / -1;
-				margin-bottom: 0;
-			}
-		}
-	}
-</style>
+<style lang="scss" scoped></style>
---
+++ Social Media Dashboard with Theme Switcher/src/components/MainApp.vue
@@ -0,0 +1,85 @@
+<script setup>
+	import SecondaryCard from "./SecondaryCard.vue"
+	import MainCard from "./MainCard.vue"
+	const props = defineProps({
+		followers: Array,
+		title: String,
+		todayOverview: Array,
+	})
+</script>
+
+<template>
+	<main class="main">
+		<section class="main__first-container">
+			<h2 class="sr-only">All Followers</h2>
+			<MainCard
+				v-for="card in props.followers"
+				:key="card.name"
+				:card="card" />
+		</section>
+		<section class="main__second-container">
+			<h3 class="main__subtitle">{{ props.title }}</h3>
+			<SecondaryCard
+				v-for="card in props.todayOverview"
+				key="card.type"
+				:card="card" />
+		</section>
+	</main>
+</template>
+
+<style lang="scss" scoped>
+	@media (min-width: 0px) {
+		.main {
+			padding: 0 1.56rem 2.81rem 1.56rem;
+			margin-top: -2.75rem;
+
+			&__first-container {
+				display: flex;
+				flex-direction: column;
+				gap: 1.5rem;
+			}
+
+			&__second-container {
+				display: flex;
+				flex-direction: column;
+				gap: 1rem;
+			}
+
+			&__subtitle {
+				color: var(--big-text);
+				font-size: 1.5rem;
+				font-weight: 700;
+				text-transform: capitalize;
+				margin-top: 2.88rem;
+				margin-bottom: calc(1.69rem - 1rem);
+			}
+		}
+	}
+
+	@media (min-width: $small-breakpoint) {
+		.main {
+			padding-bottom: 4.69rem;
+			padding-left: 10.31rem;
+			padding-right: 10.31rem;
+			margin-top: -6.75rem;
+
+			&__first-container {
+				flex-direction: row;
+				flex-wrap: wrap;
+				gap: 1.88rem;
+			}
+
+			&__second-container {
+				display: grid;
+				grid-template-columns: repeat(4, minmax(0, 1fr));
+				gap: 0;
+				row-gap: 1.5rem;
+				column-gap: 1.88rem;
+			}
+			&__subtitle {
+				grid-column: 1 / -1;
+				margin-bottom: 0;
+			}
+		}
+	}
+</style>
